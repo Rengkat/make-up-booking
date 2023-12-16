eda09f064f34ae6f11055678f1b361dc957c0905
Rengkat <successinmotion11@gmail.com>
2023-12-16 18:23:04 +0100

    add card side bar elements

--- src/app/(App)/(Private)/cart/SideBarCartList.tsx
+++ src/app/(App)/(Private)/cart/SideBarCartList.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
 import React from "react";
 
 const SideBarCartList = () => {
-  return <div>SideBarCartList</div>;
+  return (
+    <>
+      <div className="flex items-start justify-between font-montserrat pr-3 py-[1rem] border-b-[1px] border-gray-300">
+        <div className="flex gap-5">
+          <Image
+            src={"/image.png"}
+            alt="product"
+            height={500}
+            width={500}
+            className="w-[6rem] h-[7rem] object-cover bg-[#e2d7bb]"
+          />
+          <aside className="flex flex-col gap-y-2">
+            <h1>Glow DayCream</h1>
+            <h3>$175</h3>
+            <div className="flex justify-between items-center border-[1px] border-dark-gold py-2 px-3">
+              <button className="text-xl">-</button>
+              <h1>2</h1>
+              <button className="text-xl">+</button>
+            </div>
+          </aside>
+        </div>
+        <button>Remove</button>
+      </div>
+    </>
+  );
 };
 
 export default SideBarCartList;
--- src/app/(App)/components/CartModal.tsx
+++ src/app/(App)/components/CartModal.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { Fragment } from "react";
 import { IoMdClose } from "react-icons/io";
+import SideBarCartList from "../(Private)/cart/SideBarCartList";
+const cartItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const CartModal = () => {
   return (
     <div className=" fixed inset-0 z-[5] bg-transparent flex justify-end">
@@ -10,32 +12,57 @@ const CartModal = () => {
           <div> Shopping Cart (0)</div>
           <IoMdClose />
         </div>
+
         <div>
-          {/* <div className="mt-[5rem] mb-5">
-            <Image
-              src={"/cart.svg"}
-              alt="cart"
-              height={500}
-              width={500}
-              className="w-[95%] mx-auto"
-            />
-          </div>
-          <p className="w-[90%] lg:w-[70%] mx-auto text-center px-[5rem] text-[19px]">
-            Your Cart is empty. Add few items.
-          </p> */}
-          {/* <div className="flex justify-center my-[3rem]">
-            <button className="py-4 px-10 bg-dark-green text-white hover:bg-dark-gold">
-              <Link href={"/shop"}>Shop Now</Link>
-            </button>
-          </div> */}
-          <div className="flex justify-center gap-5 my-[3rem] border-t-2 border-slate-300 mx-[1rem]">
-            <button className="py-4 px-10 bg-dark-green text-white hover:bg-dark-gold">
-              <Link href={"/shop"}>Shop Now</Link>
-            </button>
-            <button className="py-4 px-10 bg-dark-gold text-white hover:bg-dark-green">
-              <Link href={"/shop"}>Shop Now</Link>
-            </button>
-          </div>
+          {cartItems.length < 1 ? (
+            <>
+              <div className="mt-[5rem] mb-5">
+                <Image
+                  src={"/cart.svg"}
+                  alt="cart"
+                  height={500}
+                  width={500}
+                  className="w-[95%] mx-auto"
+                />
+              </div>
+              <p className="w-[90%] lg:w-[70%] mx-auto text-center px-[5rem] text-[19px]">
+                Your Cart is empty. Add few items.
+              </p>
+              <div className="flex justify-center my-[3rem]">
+                <button className="py-4 px-10 bg-dark-green text-white hover:bg-dark-gold">
+                  <Link href={"/shop"}>Shop Now</Link>
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="p-5">
+                <div className="h-[55vh] overflow-auto">
+                  {cartItems.map((item) => {
+                    return (
+                      <Fragment key={item}>
+                        <SideBarCartList />
+                      </Fragment>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className=" border-t-[1px] border-slate-300 mx-[1rem] py-5">
+                <aside className="flex justify-between font-bold text-xl text-dark-green">
+                  <h1>Subtotal:</h1>
+                  <h1>$1300</h1>
+                </aside>
+                <div className="flex justify-center gap-5 my-[2rem]">
+                  <button className="py-4 px-10 bg-dark-green text-white hover:bg-dark-gold">
+                    <Link href={"/shop"}>Shop Now</Link>
+                  </button>
+                  <button className="py-4 px-10 bg-dark-gold text-white hover:bg-dark-green">
+                    <Link href={"/shop"}>Shop Now</Link>
+                  </button>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
