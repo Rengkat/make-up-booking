4286e9b0c025672b96ec5a8b3be16a86bcbf535f
Rengkat <successinmotion11@gmail.com>
2024-07-18 04:07:31 +0100

    fixed eerro

--- redux/localStorage.tsx
+++ redux/localStorage.tsx
@@ -1,35 +1,34 @@
 "use client";
 export const addTokenToLocalStorage = (token: string) => {
   if (typeof window !== "undefined") {
-    // Access localStorage here
     localStorage.setItem("token", JSON.stringify(token));
   }
 };
 
 export const removeTokenFromLocalStorage = () => {
   if (typeof window !== "undefined") {
-    // Access localStorage here
     localStorage.removeItem("token");
   }
 };
 
 export const getTokenFromLocalStorage = () => {
   if (typeof window !== "undefined") {
-    // Access localStorage here
     const result = localStorage.getItem("token");
-    const user = result ? JSON.parse(result) : null;
-    return user;
+    return result ? JSON.parse(result) : null;
   }
+  return null; // Return null or default value if window is undefined
 };
+
 export const storeGridNum = (num: number) => {
   if (typeof window !== "undefined") {
     localStorage.setItem("grid", JSON.stringify(num));
   }
 };
+
 export const getShopGrid = () => {
   if (typeof window !== "undefined") {
     const result = localStorage.getItem("grid");
-    const num = result ? JSON.parse(result) : null;
-    return num;
+    return result ? JSON.parse(result) : null;
   }
+  return null; // Return null or default value if window is undefined
 };
