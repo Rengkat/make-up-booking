3825aeca8a4cc341c808334a4b837bba4568d867
Rengkat <successinmotion11@gmail.com>
2024-10-02 16:24:25 +0100

    adjusted address

--- src/app/(App)/(Private)/account/addresses/AddressComp.tsx
+++ src/app/(App)/(Private)/account/addresses/AddressComp.tsx
@@ -1,61 +1,81 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { Fragment } from "react";
 import { MdDelete, MdEdit } from "react-icons/md";
+import { useGetUserDetailsQuery } from "../../../../../../redux/services/UserApiSlice";
 const AddressComp = () => {
+  const { isLoading, data, error, isSuccess } = useGetUserDetailsQuery({});
+  console.log(data);
+  const addresses: any = data?.user.addresses;
+  console.log(addresses);
+  {
+    isLoading && <p>Loading...</p>;
+  }
   return (
-    <div className="flex flex-col lg:flex-row lg:justify-between">
-      <aside className="lg:w-1/2">
-        <div className="my-5">
-          <label htmlFor="country" className="my-2 font-semibold">
-            Country:
-          </label>
-          <div className="text-base">Nigeria</div>
-        </div>
-        <div className="my-5">
-          <label htmlFor="country" className="my-2 font-semibold">
-            State:
-          </label>
-          <div className="text-base">Plateau State</div>
-        </div>
-        <div className="my-5">
-          <label htmlFor="country" className="my-2 font-semibold">
-            City:
-          </label>
-          <div className="text-base">Jos</div>
-        </div>
-      </aside>
-      <aside className="lg:w-1/2">
-        <div className="my-5">
-          <label htmlFor="country" className="my-2 font-semibold">
-            Town:
-          </label>
-          <div className="text-base">Mangu</div>
-        </div>
-        <div className="my-5">
-          <label htmlFor="country" className="my-2 font-semibold">
-            Nearest Landmark:
-          </label>
-          <div className="text-base">COCIN Church Sabon Barki</div>
-        </div>
-        <div className="my-5">
-          <label htmlFor="country" className="my-2 font-semibold">
-            Home Address:
-          </label>
-          <div className="text-base">No 24, Alexander Street</div>
-        </div>
-      </aside>
-      <aside>
-        <Link href={"/account/addresses/edit-address"}>
-          {" "}
-          <div className="bg-dark-gold text-white rounded size-8 grid place-content-center cursor-pointer">
-            <MdEdit fontSize={25} />
-          </div>
-        </Link>
-        <div className="bg-dark-gold text-white rounded size-8 grid place-content-center mt-5 cursor-pointer">
-          <MdDelete fontSize={25} />
-        </div>
-      </aside>
-    </div>
+    <>
+      {addresses?.map((address: any, i: number) => {
+        return (
+          <Fragment>
+            <div className="border-b-[1px] border-slate-200 my-5">
+              <h1 className="text-2xl font-light">Address {i + 1}</h1>
+              <div className="flex flex-col lg:flex-row lg:justify-between">
+                <aside className="lg:w-1/2">
+                  <div className="my-5">
+                    <label htmlFor="country" className="my-2 font-semibold">
+                      Country:
+                    </label>
+                    <div className="text-base">{address?.country}</div>
+                  </div>
+                  <div className="my-5">
+                    <label htmlFor="country" className="my-2 font-semibold">
+                      State:
+                    </label>
+                    <div className="text-base">{address?.state}</div>
+                  </div>
+                  <div className="my-5">
+                    <label htmlFor="country" className="my-2 font-semibold">
+                      City:
+                    </label>
+                    <div className="text-base">{address.city}</div>
+                  </div>
+                </aside>
+                <aside className="lg:w-1/2">
+                  <div className="my-5">
+                    <label htmlFor="country" className="my-2 font-semibold">
+                      Town:
+                    </label>
+                    <div className="text-base">{address?.town}</div>
+                  </div>
+                  <div className="my-5">
+                    <label htmlFor="country" className="my-2 font-semibold">
+                      Nearest Landmark:
+                    </label>
+                    <div className="text-base">{address?.nearestLandmark}</div>
+                  </div>
+                  <div className="my-5">
+                    <label htmlFor="country" className="my-2 font-semibold">
+                      Home Address:
+                    </label>
+                    <div className="text-base">{address.homeAddress}</div>
+                  </div>
+                </aside>
+                <aside>
+                  <Link href={"/account/addresses/edit-address"}>
+                    {" "}
+                    <div className="bg-dark-gold text-white rounded size-8 grid place-content-center cursor-pointer">
+                      <MdEdit fontSize={25} />
+                    </div>
+                  </Link>
+                  <div className="bg-dark-gold text-white rounded size-8 grid place-content-center mt-5 cursor-pointer">
+                    <MdDelete fontSize={25} />
+                  </div>
+                </aside>
+              </div>
+            </div>
+          </Fragment>
+        );
+      })}
+    </>
   );
 };
 
--- src/app/(App)/(Private)/account/addresses/page.tsx
+++ src/app/(App)/(Private)/account/addresses/page.tsx
@@ -8,13 +8,11 @@ export const metadata: Metadata = {
 const Addresses = () => {
   return (
     <div className=" bg-white p-2 lg:py-3  lg:px-[2rem] text-xl text-slate-600 mb-[5rem] shadow">
-      <div className="border-b-[1px] border-slate-200 my-5">
-        <h1 className="text-2xl font-light">Address 1</h1>
-
+      <div className=" my-5">
         <AddressComp />
       </div>
-      <h1 className="text-2xl font-light">Address 2</h1>
-      <AddressComp />
+      {/* <h1 className="text-2xl font-light">Address 2</h1> */}
+      {/* <AddressComp /> */}
       <button className="py-3 px-5 my-[1rem] bg-dark-green shadow text-white hover:bg-dark-gold">
         <Link href={"/account/addresses/add-address"}>Add Address</Link>
       </button>
