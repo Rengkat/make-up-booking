5274a7b109603a1f6c5242b2712966773d49d8b8
Rengkat <successinmotion11@gmail.com>
2024-10-09 14:07:27 +0100

    adjust booking page

--- src/app/(App)/(Private)/appointment/page.tsx
+++ src/app/(App)/(Private)/appointment/page.tsx
@@ -1,55 +1,54 @@
 import React from "react";
 import HeroComp from "../../../../components/HeroComp";
-import Image from "next/image";
 import ContactUs from "./contactUs";
-import AppointmentDate from "./DateTime";
 import AppointmentServices from "./Services";
 
 const Appointment = () => {
   return (
-    <div className="mb-[10rem]">
+    <div className="mb-[10rem] bg-light-gray">
+      {/* Hero Section */}
       <HeroComp title="Appointment" />
-      <div className="flex justify-center flex-col-reverse lg:gap-[3rem] lg:flex-row p-[1rem] mb-[5rem] lg:px-[4rem] lg:py-[7rem]">
+
+      {/* Main Content */}
+      <div className="flex flex-col-reverse lg:flex-row justify-center lg:gap-[3rem] p-[1rem] mb-[5rem] lg:px-[4rem] lg:py-[7rem]">
+        {/* Services Section */}
         <div className="w-full lg:w-[50%]">
           <AppointmentServices />
         </div>
-        <aside className="w-full lg:w-[50%] pb-5 ">
-          <div className="relative">
-            <div className="text-3xl lg:text-6xl font-normal font-bubblegum text-dark-green xl:pr-[10rem]">
+
+        {/* Working Hours Section */}
+        <aside className="w-full lg:w-[50%] pb-5">
+          <div className="relative mb-5">
+            <div className="text-4xl lg:text-6xl font-normal font-bubblegum text-dark-green">
               <span>Fullbeauty </span>
-              <span className="relative p-2">
-                <span> Working</span>
-                {/* <Image
-                  src={"/circle.svg"}
-                  alt="circle.svg"
-                  width={500}
-                  height={500}
-                  className="w-[10rem] lg:w-[18rem] xl:w-[19rem] absolute top-1 right-[0]"
-                /> */}
-              </span>
-              <span>Hours</span>
+              <span className="relative p-2 text-highlight">Working Hours</span>
             </div>
           </div>
-          <p className="py-[3rem] text-[17px] lg:text-[19px] text-slate-500 font-normal">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse animi ipsam cumque
-            consectetur nostrum tempore quasi sequi molestiae doloribus! Illum.
+
+          {/* Description */}
+          <p className="py-[1rem] text-[17px] lg:text-[19px] text-slate-600 font-normal">
+            Discover our flexible appointment hours designed to accommodate your schedule.
           </p>
-          <div className="text-[17px] text-slate-500 lg:w-[80%]">
-            <div className="flex justify-between">
-              <aside>Monday to Friday</aside>
-              <aside>10:00 am to 3:00 pm</aside>
+
+          {/* Working Hours */}
+          <div className="text-[17px] text-slate-600 lg:w-[80%] bg-white shadow-lg rounded-md p-4">
+            <div className="flex justify-between border-b border-gray-300 py-2">
+              <span>Monday to Friday</span>
+              <span>10:00 am to 3:00 pm</span>
             </div>
-            <div className="flex justify-between">
-              <aside>Saturday</aside>
-              <aside>10:00 am to 5:00 pm</aside>
+            <div className="flex justify-between border-b border-gray-300 py-2">
+              <span>Saturday</span>
+              <span>10:00 am to 5:00 pm</span>
             </div>
-            <div className="flex justify-between">
-              <aside>Sunday</aside>
-              <aside>close</aside>
+            <div className="flex justify-between py-2">
+              <span>Sunday</span>
+              <span>Closed</span>
             </div>
           </div>
         </aside>
       </div>
+
+      {/* Contact Us Section */}
       <ContactUs />
     </div>
   );
