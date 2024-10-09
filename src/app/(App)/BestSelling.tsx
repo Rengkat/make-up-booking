2e6d6cbd09a9970a4396d49290babe7c07343bf8
Rengkat <successinmotion11@gmail.com>
2024-10-09 14:42:37 +0100

    fiexes

--- src/app/(App)/BestSelling.tsx
+++ src/app/(App)/BestSelling.tsx
@@ -13,37 +13,30 @@ const BestSelling = () => {
     { pollingInterval: 50000 }
   );
   const products = data?.products || [];
-  // console.log(products);
+
   return (
     <div className="bg-white">
-      <div className="flex w-full justify-start px-5 lg:px-0 lg:justify-center text-2xl md:text-4xl lg:text-6xl font-semibold text-dark-green">
-        <div className="relative w-[5rem] md:w-[12rem] lg:w-[10%] xl:w-[12%] mb-[2rem] lg:mb-[0rem] ">
-          <span>Best </span>
-          <Image
-            src={"/circle.svg"}
-            width={500}
-            height={500}
-            alt="leaf"
-            className="absolute top-[0.2rem] lg:-top-[0rem] md:-top-[1rem] -left-[10px] lg:-left-[0.5rem] xl:-left-[1.5rem] z-[5] w-[100%] md:w-[100%] lg:w-[100%] "
-          />
+      <header className="flex flex-col items-center mb-8">
+        <div className="relative w-[5rem] md:w-[12rem] lg:w-[10%] xl:w-[12%] mb-[1rem]">
+          <span className="text-2xl md:text-4xl lg:text-6xl font-semibold text-dark-green">
+            Best{" "}
+          </span>
         </div>
-        <span>Selling Products</span>
-      </div>
-      <div className="px-[1rem] lg:px-[5rem] py-[2rem] lg:py-[5rem] bg-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6  gap-y-[3rem]">
+        <span className="text-2xl md:text-4xl lg:text-6xl font-semibold text-dark-green">
+          Selling Products
+        </span>
+      </header>
+      <div className="px-[1rem] lg:px-[5rem] py-[2rem] lg:py-[5rem] bg-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-[3rem]">
         {isLoading ? (
           <div className="w-full flex justify-center h-[50vh] items-center">
-            <p>Loading...</p>
+            <p className="font-semibold text-xl text-gray-700">Loading...</p>
           </div>
         ) : (
-          <>
-            {products.slice(0, 8).map((product: ProductType) => {
-              return (
-                <Fragment key={product?._id}>
-                  <Product product={product} />
-                </Fragment>
-              );
-            })}
-          </>
+          products.slice(0, 8).map((product: ProductType) => (
+            <Fragment key={product?._id}>
+              <Product product={product} />
+            </Fragment>
+          ))
         )}
       </div>
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,27 +4,30 @@ import React from "react";
 const Hero = () => {
   return (
     <>
-      <div id="top" className=" flex justify-center px-[2rem] py-[5rem] ">
-        <div className="w-full lg:w-[80%] lg:mt-[2rem] flex flex-col-reverse lg:flex-row justify-center gap-[6rem]">
-          <div className="relative">
-            <div className="w-[90%] lg:w-[25rem] mx-auto lg:mx-0 relative z-[2] h-[20rem] lg:h-[25rem] bg-dark-green"></div>
+      <section id="top" className="flex justify-center px-4 py-20 bg-light-green">
+        <div className="w-full lg:w-[80%] flex flex-col-reverse lg:flex-row justify-center gap-16 lg:gap-32">
+          {/* Left Side: Images */}
+          <div className="relative flex items-center">
+            <div className="w-[90%] lg:w-[25rem] mx-auto lg:mx-0 relative z-[2] h-[20rem] lg:h-[25rem] bg-dark-green rounded-lg shadow-lg"></div>
             <Image
               src={"/left-1.png"}
               width={500}
               height={500}
               alt="leaf"
-              className="absolute w-[60%] top-5 - lg:-top-0 -left-[5rem] lg:-left-[7rem] "
+              className="absolute w-[60%] top-5 lg:-top-0 -left-[5rem] lg:-left-[7rem] transition-transform duration-300"
             />
             <Image
               src={"/vertical.png"}
               width={500}
               height={500}
               alt="leaf"
-              className="absolute -top-[8rem] z-[5]"
+              className="absolute -top-32 z-[5] transition-transform duration-300"
             />
           </div>
-          <div className="text-7xl mt-[1rem] lg:text-[8rem] font-montserrat text-dark-green">
-            <h1 className=" p-0">Be </h1>
+
+          {/* Right Side: Text */}
+          <div className="text-7xl mt-4 lg:text-[8rem] font-montserrat text-dark-green leading-tight">
+            <h1 className="p-0">Be </h1>
             <div className="relative lg:w-[40rem]">
               <h1 className="my-5">Gorgeous!</h1>
               <Image
@@ -32,35 +35,34 @@ const Hero = () => {
                 width={500}
                 height={500}
                 alt="leaf"
-                className="absolute -top-[1rem] lg:-top-[2rem] -left-[5px] lg:-left-[1rem] z-[2] w-[100%] md:w-[45%] lg:w-[100%] "
+                className="absolute -top-[1rem] lg:-top-[2rem] -left-2 lg:-left-1 z-[2] w-[100%] md:w-[45%] lg:w-[100%] transition-transform duration-300"
               />
             </div>
           </div>
         </div>
-      </div>
+      </section>
+
       <div className="flex justify-end">
-        <div className="w-full lg:w-[60%] py-[5rem] px-[2rem] flex flex-col lg:flex-row justify-center gap-[5rem] items-center mb-5 font-dancing bg-dark-green">
-          <div className="w-full lg:w-[40%] text-white">
-            <h1 className="text-4xl ">Shop More </h1>
-            <p className="text-[18px] py-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
+        <div className="w-full lg:w-[60%] py-20 px-4 flex flex-col lg:flex-row justify-center gap-16 items-center mb-5 font-dancing bg-dark-green rounded-lg shadow-lg">
+          <div className="w-full lg:w-[40%] text-white text-center lg:text-left">
+            <h1 className="text-4xl">Shop More </h1>
+            <p className="text-lg py-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
           <div className="relative w-[60%]">
-            <div className="w-[12rem] h-[12rem] border-[1px] border-white flex justify-center">
+            <div className="w-[12rem] h-[12rem] border-[1px] border-white flex justify-center items-center overflow-hidden rounded-full shadow-md">
               <Image
                 src={"/Image-1-min.png"}
                 alt="product"
                 height={500}
                 width={500}
-                className="w-[100%]"
+                className="w-full h-full object-cover"
               />
               <Image
                 src={"/white-leaf.webp"}
                 alt="product"
                 height={500}
                 width={500}
-                className="w-[25%] absolute bottom-5 -left-[3rem]"
+                className="w-[25%] absolute bottom-5 -left-[3rem] transition-transform duration-300"
               />
             </div>
           </div>
